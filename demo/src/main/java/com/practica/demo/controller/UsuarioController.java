9ff1341b01679bd15e92e1ee69396d40d1e4bdee
fmoutinhodev <moutinho132@gmail.com>
2024-09-12 21:37:38 -0400

    Collection postman y Controller

--- demo/src/main/java/com/practica/demo/controller/UsuarioController.java
+++ demo/src/main/java/com/practica/demo/controller/UsuarioController.java
@@ -5,10 +5,10 @@ import com.practica.demo.service.UsuarioServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/usuario")
@@ -20,5 +20,34 @@ public class UsuarioController {
     public ResponseEntity<UsuarioDto> createUsuario(@RequestBody UsuarioDto usuarioDto) {
         UsuarioDto createdUsuario = service.createUsuario(usuarioDto);
         return new ResponseEntity<>(createdUsuario, HttpStatus.CREATED);
+    }
+
+    // Obtener un usuario por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioDto> getUsuarioById(@PathVariable Long id) {
+        Optional<UsuarioDto> usuarioDto = service.getUsuarioById(id);
+        return usuarioDto.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Obtener todos los usuarios
+    @GetMapping
+    public ResponseEntity<List<UsuarioDto>> getAllUsuarios() {
+        List<UsuarioDto> usuarios = service.ObtenerListadoUsuarios();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    // Actualizar un usuario por ID
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioDto> updateUsuario(@PathVariable Long id, @RequestBody UsuarioDto usuarioDto) {
+        UsuarioDto updatedUsuario = service.updateUsuario(id, usuarioDto);
+        return ResponseEntity.ok(updatedUsuario);
+    }
+
+    // Eliminar un usuario por ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable Long id) {
+        service.deleteUsuario(id);
+        return ResponseEntity.noContent().build();
     }
 }
